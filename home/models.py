5df605b12aa6f804250b9ec63231ca1921de19bd
abdelrahman-a99 <a.ahmed2299>
2024-11-28 02:57:39 +0200

    migrate database

--- home/models.py
+++ home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+# pylint: disable=no-member
 class ContactSubmission(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
