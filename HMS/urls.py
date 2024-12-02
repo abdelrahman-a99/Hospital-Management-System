134bab403ce1826b3fd2bd8ecb013c6542ff4813
malak <malaksh2023@outlook.com>
2024-12-02 19:58:41 +0200

    trying to change stuff

--- HMS/urls.py
+++ HMS/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", include("home.urls")),
     path("Accounts/", include("Accounts.urls")),
     path("doctor/", include("doctor.urls")),
+    path("patient/", include("patient.urls")),
     path('appointments/', include('appointments.urls')),  # This should work if appointments.urls exists
 ]
